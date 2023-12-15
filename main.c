e5e17ff9e72db98e3cd1cc6cc35afd4fa76d89a5
anton-tchekov <anton@tchekov.net>
2023-12-15 17:18:30 +0000

    Tabs

--- main.c
+++ main.c
@@ -9,15 +9,15 @@ int main(void)
 	led_pixel(0, 0, 255, 0, 0);
 	led_update();
 
-    // TODO: Define Board Structure
-    // TODO: Generate Board with mines
+	// TODO: Define Board Structure
+	// TODO: Generate Board with mines
 
 	for(;;)
 	{
-        // TODO: Check for Cursor input
-        // TODO: Handle cursor action (dig/flag)
-        // TODO: Update Gamestate
-        // TODO: Render Game
+		// TODO: Check for Cursor input
+		// TODO: Handle cursor action (dig/flag)
+		// TODO: Update Gamestate
+		// TODO: Render Game
 	}
 
 	return 0;
