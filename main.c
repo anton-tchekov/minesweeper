104ab95b57b2ac57f7fbe040649974d1521b69da
Tim Gabrikowski <tim@gabrikowski.de>
2023-12-15 18:14:00 +0100

    update .gitignore and add simple programm structure

--- main.c
+++ main.c
@@ -9,8 +9,15 @@ int main(void)
 	led_pixel(0, 0, 255, 0, 0);
 	led_update();
 
+    // TODO: Define Board Structure
+    // TODO: Generate Board with mines
+
 	for(;;)
 	{
+        // TODO: Check for Cursor input
+        // TODO: Handle cursor action (dig/flag)
+        // TODO: Update Gamestate
+        // TODO: Render Game
 	}
 
 	return 0;
