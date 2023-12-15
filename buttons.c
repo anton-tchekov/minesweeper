4b67cc6a336bb2065fa3f848bb3b74c345185440
anton-tchekov <anton@tchekov.net>
2023-12-15 19:09:31 +0000

    broken

--- buttons.c
+++ buttons.c
@@ -1,6 +1,43 @@
-static uint16_t buttons_scan(void)
+#define BUTTON_DEBOUNCE 15
+
+static void button_event(uint8_t idx);
+
+static uint8_t states[16];
+
+static void buttons_init(void)
 {
-	
+	PORTB |= 0x0F;
 }
 
+static void buttons_scan(void)
+{
+	uint8_t row, col, val, idx;
+	idx = 0;
+	for(row = 0; row < 4; ++row)
+	{
+		DDRC |= (1 << row);
+		_delay_ms(1);
+		val = PINB & 0x0F;
+		DDRC &= (1 << row);
 
+		for(col = 0; col < 4; ++col, ++idx)
+		{
+			if(val & (1 << col))
+			{
+				states[idx] = 0;
+			}
+			else
+			{
+				if(states[idx] < BUTTON_DEBOUNCE)
+				{
+					++states[idx];
+				}
+				else
+				{
+					states[idx] = 0;
+					button_event(idx);
+				}
+			}
+		}
+	}
+}
--- main.c
+++ main.c
@@ -1,19 +1,68 @@
 #include <stdint.h>
 #include <string.h>
 #include <avr/io.h>
+#include <util/delay.h>
 #include "ws2812.c"
+#include "buttons.c"
+
+enum
+{
+	BTN_LEFT  = 4,
+	BTN_RIGHT = 6,
+	BTN_UP    = 1,
+	BTN_DOWN  = 9
+};
+
+static uint8_t cx, cy;
+
+static void button_event(uint8_t idx)
+{
+	if(idx == BTN_LEFT)
+	{
+		if(cx > 0) { --cx; }
+	}
+
+	if(idx == BTN_RIGHT)
+	{
+		if(cx < LED_WIDTH - 1) { ++cx; }
+	}
+
+	if(idx == BTN_UP)
+	{
+		if(cy > 0) { --cy; }
+	}
+
+	if(idx == BTN_DOWN)
+	{
+		if(cy < LED_HEIGHT - 1) { ++cy; }
+	}
+
+	led_clear();
+	led_pixel(idx % 4, idx / 4, 255, 0, 0);
+	led_update();
+}
 
 int main(void)
 {
+	buttons_init();
+
 	led_clear();
-	led_pixel(0, 0, 255, 0, 0);
 	led_update();
 
 	// TODO: Define Board Structure
 	// TODO: Generate Board with mines
 
+	cx = 0;
+	cy = 0;
+
 	for(;;)
 	{
+		buttons_scan();
+
+		/*led_clear();
+		led_pixel(cx, cy, 0, 0, 255);
+		led_update();*/
+
 		// TODO: Check for Cursor input
 		// TODO: Handle cursor action (dig/flag)
 		// TODO: Update Gamestate
